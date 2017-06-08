e2943d25b058a97b91639f933e76516dea3c0d5f
witekbobrowski <witek@bobrowski.com.pl>
2017-06-08 16:33:54 +0200

    Initial Commit

---
+++ EPUBKit/EPUBKit.h
@@ -0,0 +1,19 @@
+//
+//  EPUBKit.h
+//  EPUBKit
+//
+//  Created by Witek on 08/06/2017.
+//  Copyright © 2017 Witek Bobrowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EPUBKit.
+FOUNDATION_EXPORT double EPUBKitVersionNumber;
+
+//! Project version string for EPUBKit.
+FOUNDATION_EXPORT const unsigned char EPUBKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EPUBKit/PublicHeader.h>
+
+
