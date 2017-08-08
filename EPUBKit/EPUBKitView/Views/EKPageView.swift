a0b43d334face465ef6ce8f115cde56758faa2b9
witekbobrowski <witek@bobrowski.com.pl>
2017-08-08 23:38:23 +0200

    Separated EKPageView from EKViewController

---
+++ EPUBKit/EPUBKitView/Views/EKPageView.swift
@@ -0,0 +1,21 @@
+//
+//  EKPageView.swift
+//  EPUBKit
+//
+//  Created by Witek on 08/08/2017.
+//  Copyright © 2017 Witek Bobrowski. All rights reserved.
+//
+
+import UIKit
+
+class EKPageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
