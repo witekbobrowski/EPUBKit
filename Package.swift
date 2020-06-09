0f5c871bb8bf7a169cd7b97e1e4f77172ad74ddc
Mykola Stukalo <mykola.stukalo@icloud.com>
2020-06-09 11:48:51 +0300

    added platforms, products, targets to Package.swift

--- Package.swift
+++ Package.swift
@@ -3,5 +3,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "EPUBKit"
+    name: "EPUBKit",
+    
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v9),
+        .tvOS(.v9)
+    ],
+    
+    products: [
+        .library(name: "EPUBKit", targets: ["EPUBKit"]),
+    ],
+    
+    targets: [
+        .target(
+            name: "EPUBKit",
+            path: "Sources")
+    ]
 )
