77c0d0be41faba971e68b09a8bf56264ed02f96e
Mykola Stukalo <mykola.stukalo@icloud.com>
2020-06-09 11:59:14 +0300

    added Zip as package dependency

--- Package.swift
+++ Package.swift
@@ -15,12 +15,17 @@ let package = Package(
         .library(name: "EPUBKit", targets: ["EPUBKit"]),
     ],
     
-    dependencies: [.package(url: "https://github.com/tadija/AEXML", from: "4.5.0")],
+    dependencies: [
+        .package(
+            url: "https://github.com/tadija/AEXML",
+            from: "4.5.0"),
+        .package(url: "https://github.com/marmelroy/Zip", from: "2.0.0")
+    ],
     
     targets: [
         .target(
             name: "EPUBKit",
-            dependencies: ["AEXML"],
+            dependencies: ["AEXML", "Zip"],
             path: "Sources"
         )
     ]
